0199fe6c8f2efa9916422883d0ee5e10880f81f0
institutfrancaistest <theophile.vast@institutfrancais.es>
2017-02-27 12:03:25 +0100

    add a progress bar

---
+++ step.js
@@ -0,0 +1,28 @@
+var i="1"
+
+function progressBar() {
+	x= i++*10
+	document.getElementById("progressbar").style.width= x+"%";
+	console.log(x);
+	}
+
+
+/*function toggle() {
+
+$( "numero prescedent" ).toggleClass( numero suivant, addOrRemove );
+i++
+$(progressbar).style.widt= 100/arraylength
+}
+
+function progressBar () {
+	$( "no,bouton" ).click(function() {
+  $( "progressbar" ).animate({
+    opacity: 0.25,
+    left: "+=50",
+    height: "toggle"
+  }, 5000, function() {
+    // Animation complete.
+  });
+});
+}*/
+
