d3c7fbeccc3b7ea88176881639217d2f9687da0f
institutfrancaistest <theophile.vast@institutfrancais.es>
2017-03-06 17:38:00 +0100

    fix array issuses 2

--- turfu.js
+++ turfu.js
@@ -15,7 +15,8 @@ function toggleCv() {
   //collecte des réponses
   for (i = 0; i < x.length ;i++) {
     // text += x.elements[i].value 
-    array.push(x.elements[i].value)    
+    array.push(x.elements[i].value) 
+    console.log(array)   
     }
     // ecriture de la lettre avec differentes variables
       /*  var adresse= "";
@@ -34,11 +35,11 @@ function toggleCv() {
     text2 = ("<center><strong>" +array[5]+ "</center></strong><br> ");
     document.getElementById("title").innerHTML = text2;
     text3 = ("<strong><br>FORMATION PROFESSIONNELLE</strong><br><br> " +array[6]+"<br>"+ "<br><strong>EXPERIENCE</strong><br><br> ");
-    text3 = (text3 + "  " +array[5]+ " (de 2000 à 2009) <br>" +array[15]+ " (de 2010 à 2016)<strong><br><br>LANGUE</strong><br><br> " +array[8]+ " courant<br>");
+    text3 = (text3 + "  " +array[7]+ " (de 2000 à 2009) <br>" +array[15]+ " (de 2010 à 2016)<strong><br><br>LANGUE</strong><br><br> " +array[8]+ " courant<br>");
     text3 = (text3 + " <br><strong>LOISIRS</strong><br><br> " +array[9]+ "<br>J'aime aussi manger des " +array[10]+ " et du " +array[11]+ " au soleil<br> ");
     text3 = (text3 + " <br><strong>MES ATOUTS</strong><br><br>" + "J'ai animé l'émission de télévision " + array[13]+ " pendant trois ans.<br><br> ");
     text3 = (text3 + "Jai écrit une biographie de " + array[14] + " parue le " + array[16]  );
-    text3 = (text3 +" <br>Mes expériences de "+array[5]+" et de "+array[15]+" ainsi que mes connaissances en "+array[9]+" me permettent donc de prétendre à un poste de "+array[5]+"<br>");
+    text3 = (text3 +" <br>Mes expériences de "+array[7]+" et de "+array[15]+" ainsi que mes connaissances en "+array[9]+" me permettent donc de prétendre à un poste de "+array[5]+"<br>");
     text3 = (text3 +"<br>Pour ce poste mes prétentions sont de recevoir un salaire net mensuel de " +array[12]+ " par mois! ");
    //ajoute le texte dans la balise p avec l'id demo1
     document.getElementById("sub").innerHTML = text3;
