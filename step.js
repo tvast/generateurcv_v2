285f11c187c338aac7d055f30ce9c3b82b053844
institutfrancaistest <theophile.vast@institutfrancais.es>
2017-03-15 17:38:47 +0100

    add inverse progress bar

--- step.js
+++ step.js
@@ -6,14 +6,13 @@ var t = 0;
 function progressBar() {
 
 	/*calcul % avance form*/
+
 	var x = document.getElementById("frm1");
 	var h = x.length;
-	console.log(h+"tab")
 	var foo = 100/h;
-	/*console.log(foo)*/
-	/*Iteration a chaque click*/
-	x= i++*10;
+	
 	document.getElementById("progressbar").style.width= foo*g+"%";
+
 	/*switch de question*/
 
 	if (g == 0) {
@@ -28,37 +27,17 @@ function progressBar() {
 	else {
 		var p = g++;
 		var t = p-1;
-		console.log(p+"");
-		console.log(t+"-1");
-		console.log(g+"zini");
-		/*document.getElementById(0).classList.add("toggleCv");
-		document.getElementById(1).classList.add("toggleCv");*/
 		document.getElementById(t).classList.toggle("toggleCv");
 		document.getElementById(p).classList.toggle("toggleCv");
 	};
 	
 }
+function progressBar2() {
+	/*calcul % recule form*/
 
-/*function switchQh () {
+	var x = document.getElementById("frm1");
+	var h = x.length;
+	var foo = 100/h;
 	
-	if (g == 0) {
-		var f = g++;
-		console.log(f+"init");
-
-		
-		document.getElementById(g).classList.toggle("toggleCv");
-		document.getElementById(f).classList.toggle("toggleCv");
-	}
-	else {
-		var p = g++;
-		var t = p-1;
-	/*	console.log(p+"+1");
-		console.log(t+"-1");
-		console.log(g+"zini");
-		
-		document.getElementById(t).classList.toggle("toggleCv");
-		document.getElementById(p).classList.toggle("toggleCv");
-	};
-}*/
-
-
+	document.getElementById("progressbar").style.width= (foo*g)-h+"%";
+}
