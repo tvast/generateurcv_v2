fafe7936c26d7f2b02a0ed25793a0d42eea93e9b
theo vast <tvast>
2017-03-29 15:32:39 +0200

    change cv style

--- generator.js
+++ generator.js
@@ -29,22 +29,44 @@ function toggleCv() {
     text = ("");
     text = (text + " " +array[0]+ " ");
     text = (text + " " +array[1]);
+
     document.getElementById("head1").innerHTML = text;
+
     text1 =  ("<br>12 rue " +array[2]+ " ");
     text1 = (text1 + " | " +array[3]+ " | ");
     text1 = (text1 +" | " +array[4]+ " ans");
+
     document.getElementById("head2").innerHTML = text1;
+
     text2 = (array[5]);
+
     document.getElementById("title").innerHTML = text2;
-    text3 = ("<strong><br>FORMATION PROFESSIONNELLE</strong><br><br> " +array[6]+"<br>"+ "<br><strong>EXPERIENCE</strong><br><br> ");
-    text3 = (text3 + "  " +array[7]+ " (de 2000 à 2009) <br>" +array[15]+ " (de 2010 à 2016)<strong><br><br>LANGUE</strong><br><br> " +array[8]+ " courant<br>");
-    text3 = (text3 + " <br><strong>LOISIRS</strong><br><br> " +array[9]+ "<br>J'aime aussi manger des " +array[10]+ " et du " +array[11]+ " au soleil<br> ");
-    text3 = (text3 + " <br><strong>MES ATOUTS</strong><br><br>" + "J\'ai animé l'émission de télévision " + array[13]+ " pendant trois ans.<br><br> ");
-    text3 = (text3 + "J\'ai écrit une biographie de " + array[14] + " parue le " + array[16]  );
-    text3 = (text3 +" <br>Mes expériences de "+array[7]+" et de "+array[15]+" ainsi que mes connaissances en "+array[8]+" me permettent donc de prétendre à un poste de "+array[5]+"<br>");
-    text3 = (text3 +"<br>Pour ce poste mes prétentions sont de recevoir un salaire net mensuel de " +array[12]+ " par mois! ");
+
+    text3 = (array[6]);
+
+    document.getElementById("fp").innerHTML = text3;
+
+    text4 = (array[7]+ " (de 2000 à 2009) <br>" +array[15]+ " (de 2010 à 2016)<strong><br><br>LANGUE</strong><br><br> " +array[8]+ " courant<br>");
+    
+    document.getElementById("ex").innerHTML = text4;
+
+    text5 = (array[9]+ "<br>J'aime aussi manger des " +array[10]+ " et du " +array[11]+ " au soleil<br> ");
+    
+    document.getElementById("lg").innerHTML = text5;
+
+    text6 = ("J\'ai animé l'émission de télévision " + array[13]+ " pendant trois ans.<br><br> ");
+    
+    document.getElementById("ls").innerHTML = text6;
+
+    text7 = ("J\'ai écrit une biographie de " + array[14] + " parue le " + array[16]  );
+    
+    document.getElementById("ma").innerHTML = text7;
+
+    text8 = (" <br>Mes expériences de "+array[7]+" et de "+array[15]+" ainsi que mes connaissances en "+array[8]+" me permettent donc de prétendre à un poste de "+array[5]+"<br>");
+    text8 = (text8 +"<br>Pour ce poste mes prétentions sont de recevoir un salaire net mensuel de " +array[12]+ " par mois! ");
    //ajoute le texte dans la balise p avec l'id demo1
-    document.getElementById("sub").innerHTML = text3;
+    
+    document.getElementById("sub").innerHTML = text8;
 
               }
 
